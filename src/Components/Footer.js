ac60ffdf034b7f5c5a6abc6738f9602cdc6163f3
hadeelhhawajreh <hadeelhhawajreh@gmail.com>
2021-05-26 14:34:34 +0300

    testing for delopying in netfliy 2

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,10 +2,19 @@ import React from "react";
 
 class Footer extends React.Component {
     render() {
+        const mystyle = {
+            color: "white",
+            backgroundColor: "DodgerBlue",
+            padding: "10px",
+            fontFamily: "Arial",
+            textAlign:'center'
+          };
+
+          
         return (
             <>
                 <footer>
-                    <h5> Copy Right bY Hadeel </h5>
+                    <h5 style={mystyle}> Copy Right bY Hadeel </h5>
 
                 </footer>
             </>
